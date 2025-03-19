86bb5552415e64887380c6226f10ce6f6bd6de11
José Ramírez Dure <132848165+joseram03@users.noreply.github.com>
2025-03-19 10:15:51 -0300

    Capa Servicio de Estado

---
+++ src/main/java/com/crud/trello_mj/estado/EstadoServicio.java
@@ -0,0 +1,32 @@
+package com.crud.trello_mj.estado;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import java.util.List;
+
+@Stateless
+public class EstadoServicio {
+
+    @EJB
+    private EstadoRepositorio estadoRepositorio;
+
+    public void guardar(Estado estado) {
+        if (estado.getId() == null) {
+            estadoRepositorio.crear(estado);
+        } else {
+            estadoRepositorio.actualizar(estado);
+        }
+    }
+
+    public void eliminar(Long id) {
+        estadoRepositorio.eliminar(id);
+    }
+
+    public Estado buscarPorId(Long id) {
+        return estadoRepositorio.buscarPorId(id);
+    }
+
+    public List<Estado> listarTodos() {
+        return estadoRepositorio.listarTodos();
+    }
+}
