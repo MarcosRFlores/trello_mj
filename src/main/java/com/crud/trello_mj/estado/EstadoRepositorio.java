47f417dcc0acab7b20631e5ffc3d1433f3c11785
José Ramírez Dure <132848165+joseram03@users.noreply.github.com>
2025-03-19 10:15:33 -0300

    Capa repositorio de Estado

---
+++ src/main/java/com/crud/trello_mj/estado/EstadoRepositorio.java
@@ -0,0 +1,37 @@
+package com.crud.trello_mj.estado;
+
+import com.crud.trello_mj.estado.Estado;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Stateless
+public class EstadoRepositorio {
+
+    @PersistenceContext(unitName = "jsf-crud-unit")
+    private EntityManager entityManager;
+
+    public void crear(Estado estado) {
+        entityManager.persist(estado);
+    }
+
+    public void actualizar(Estado estado) {
+        entityManager.merge(estado);
+    }
+
+    public void eliminar(Long id) {
+        Estado estado = entityManager.find(Estado.class, id);
+        if (estado != null) {
+            entityManager.remove(estado);
+        }
+    }
+
+    public Estado buscarPorId(Long id) {
+        return entityManager.find(Estado.class, id);
+    }
+
+    public List<Estado> listarTodos() {
+        return entityManager.createQuery("SELECT e FROM Estado e", Estado.class).getResultList();
+    }
+}
