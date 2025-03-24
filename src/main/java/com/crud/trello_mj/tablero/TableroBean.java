f081427229e195918e7e0c60d41ef027c900f6b7
José Ramírez Dure <132848165+joseram03@users.noreply.github.com>
2025-03-24 11:19:33 -0300

    Implementacion del tablero v1.0
    El tablero de manera Funcional

--- src/main/java/com/crud/trello_mj/tablero/TableroBean.java
+++ src/main/java/com/crud/trello_mj/tablero/TableroBean.java
@@ -1,14 +1,18 @@
 package com.crud.trello_mj.tablero;
 
+import com.crud.trello_mj.estado.EstadoBean;
 import com.crud.trello_mj.tarea.Tarea;
 import com.crud.trello_mj.tarea.TareaBean;
 import com.crud.trello_mj.tarea.TareaServicio;
 import com.crud.trello_mj.usuario.Usuario;
 import com.crud.trello_mj.usuario.UsuarioBean;
 
+import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 
@@ -19,13 +23,70 @@ public class TableroBean implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Inject
-    private UsuarioBean usuarioBean;
+    private TareaBean tareaBean;
 
     @Inject
-    private TareaBean tareaBean;
+    private EstadoBean estadoBean;
+
+    // Usuario Logueado
+    private Usuario usuario;
 
     // Los 3 tableros
     private List<Tarea> tareasPendientes;
     private List<Tarea> tareasEnCurso;
-    private List<Tarea> tareasFinalizada;
+    private List<Tarea> tareasFinalizadas;
+
+    public List<Tarea> getTareasPendientes() {
+        return tareasPendientes;
+    }
+
+    public void setTareasPendientes(List<Tarea> tareasPendientes) {
+        this.tareasPendientes = tareasPendientes;
+    }
+
+    public List<Tarea> getTareasEnCurso() {
+        return tareasEnCurso;
+    }
+
+    public void setTareasEnCurso(List<Tarea> tareasEnCurso) {
+        this.tareasEnCurso = tareasEnCurso;
+    }
+
+    public List<Tarea> getTareasFinalizadas() {
+        return tareasFinalizadas;
+    }
+
+    public void setTareasFinalizadas(List<Tarea> tareasFinalizadas) {
+        this.tareasFinalizadas = tareasFinalizadas;
+    }
+
+    @PostConstruct
+    public void init() {
+        FacesContext context = FacesContext.getCurrentInstance();
+
+        usuario = (Usuario) context.getExternalContext().getSessionMap().get("usuario");
+
+        if (usuario == null) {
+            try {
+                context.getExternalContext().redirect("login.xhtml");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            cargarTareas();
+        }
+    }
+
+    public void cargarTareas() {
+        // Asumimos que los estados tienen IDs fijos: 1=Pendiente, 2=En curso, 3=Finalizada
+        tareasPendientes = tareaBean.listarPorUsuarioYEstado(usuario.getId(), 1L);
+        tareasEnCurso = tareaBean.listarPorUsuarioYEstado(usuario.getId(), 2L);
+        tareasFinalizadas = tareaBean.listarPorUsuarioYEstado(usuario.getId(), 3L);
+    }
+
+    public void cambiarEstado(Long idTarea, Long idEstado) {
+        estadoBean.cargarEstado(idEstado);
+        tareaBean.cambiarEstado(idTarea, estadoBean.getEstado());
+        cargarTareas();
+    }
 }
